dd0c33e1ff74cfd459409744465f314547b7b48a
Chrisitian Carl C. Cabalde <cccabalde30858@liceo.edu.ph>
2025-08-04 20:14:38 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        
+        Book book1 = new Book("Mistborn", "Brandon Sanderson", 400, true);
+        book1.displayInfo();
     }
 }
