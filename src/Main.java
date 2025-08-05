4332dff3e35b589b92249935d97557cd4ed20366
Chrisitian Carl C. Cabalde <cccabalde30858@liceo.edu.ph>
2025-08-05 11:36:02 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        Book book1 = new Book("Mistborn", "Brandon Sanderson", 400, true);
+        Book book1 = new Book("48 Laws of Power", "Robert Greene", 300, true);
         book1.displayInfo();
     }
 }
