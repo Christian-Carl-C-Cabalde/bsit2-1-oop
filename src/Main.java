53e6a96a54c8d3e26b444432da8b7e59265486e0
Chrisitian Carl C. Cabalde <cccabalde30858@liceo.edu.ph>
2025-08-19 12:13:31 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bank Name: " + BankAccount.bankName);
-        System.out.println("Interest Rate: " + (BankAccount.interestRate * 100) + "%\n");
+        System.out.println("Bank Name: " + bankAccount.bankName);
+        System.out.println("Interest Rate: " + (bankAccount.interestRate * 100) + "%\n");
 
-        BankAccount account1 = new BankAccount("Cabalde", 1000);
-        BankAccount account2 = new BankAccount("Johnden", 2500);
-        BankAccount account3 = new BankAccount("Ashton", 500);
+        bankAccount account1 = new bankAccount("Cabalde", 1000);
+        bankAccount account2 = new bankAccount("Johnden", 2500);
+        bankAccount account3 = new bankAccount("Ashton", 500);
 
         System.out.println("\n═══ Account Operations ═══");
         account1.deposit(500);
