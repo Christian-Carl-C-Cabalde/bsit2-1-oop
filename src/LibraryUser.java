d83c439e9f531f0d0b4588979de3204c7e62a710
Christian Carl C. Cabalde <cccabalde30858@liceo.edu.ph>
2025-09-23 12:02:41 +0800

    lab-activity-11

---
+++ src/LibraryUser.java
@@ -0,0 +1,115 @@
+// LibraryUser.java
+interface LibraryUser {
+    String getUserType();
+    void borrowMedia(Media media);
+    void returnMedia(Media media);
+    int getMaxBorrowLimit();
+}
+
+// Student.java
+class Student implements LibraryUser {
+    private String name;
+    private String studentId;
+
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String getUserType() {
+        return "Student";
+    }
+
+    @Override
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available.");
+        }
+    }
+
+    @Override
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+
+    @Override
+    public int getMaxBorrowLimit() {
+        return 3;
+    }
+}
+
+// Teacher.java
+class Teacher implements LibraryUser {
+    private String name;
+    private String employeeId;
+
+    public Teacher(String name, String employeeId) {
+        this.name = name;
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String getUserType() {
+        return "Teacher";
+    }
+
+    @Override
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available.");
+        }
+    }
+
+    @Override
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+
+    @Override
+    public int getMaxBorrowLimit() {
+        return 5;
+    }
+}
+
+// Librarian.java
+class Librarian implements LibraryUser {
+    private String name;
+    private String employeeId;
+
+    public Librarian(String name, String employeeId) {
+        this.name = name;
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String getUserType() {
+        return "Librarian";
+    }
+
+    @Override
+    public void borrowMedia(Media media) {
+        if (media.isAvailable()) {
+            media.borrowItem();
+            System.out.println(name + " (" + getUserType() + ") borrowed: " + media.getTitle());
+        } else {
+            System.out.println("Sorry, " + media.getTitle() + " is not available.");
+        }
+    }
+
+    @Override
+    public void returnMedia(Media media) {
+        media.returnItem();
+    }
+
+    @Override
+    public int getMaxBorrowLimit() {
+        return 10;
+    }
+}
